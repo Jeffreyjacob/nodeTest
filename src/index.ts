11ce5ca1ea7fd8c1b2c7f6f73834146d525929f3
jeffrey <jeffreyigiemeh16@gmail.com>
2025-04-03 23:51:25 -0700

    fixing apollo graphql playground to run on production

--- src/index.ts
+++ src/index.ts
@@ -1,38 +1,48 @@
 import { ApolloServer } from "@apollo/server";
-import express, { Express} from "express"
+import express, { Express } from "express"
 import { expressMiddleware } from "@apollo/server/express4";
 import http from 'http'
 import dotenv from "dotenv";
+import cors from "cors"
 import { ApolloServerPluginDrainHttpServer } from "@apollo/server/plugin/drainHttpServer"
 import { ConnectDB } from "./db/connect"
 import mergedTypeDefs from "./Schema/index";
+import playground from 'graphql-playground-middleware-express';
 import mergedResolver from "./resolvers/index";
 
 dotenv.config()
 
 const app: Express = express()
 const httpServer = http.createServer(app)
-const PORT = process.env.PORT || 5000 
+const PORT = process.env.PORT || 5000
 
 const startServer = async () => {
     const server = new ApolloServer({
         typeDefs: mergedTypeDefs,
         resolvers: mergedResolver,
         plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
+        introspection: true,
+        
     })
 
-await server.start();
-app.use('/graphql',
+    await server.start();
+    app.use('/graphql',
+        cors({
+            origin: 'https://nodetest-wgfx.onrender.com', 
+            allowedHeaders: 'Content-Type, Authorization',
+            credentials:true
+        }),
         express.json(),
-        express.urlencoded({extended:true}),
-        expressMiddleware(server,{
-            context: async ({ req,res}) => ({req,res}),
+        express.urlencoded({ extended: true }),
+        expressMiddleware(server, {
+            context: async ({ req, res }) => ({ req, res }),
         }),
     );
-
-await new Promise<void>(resolve => httpServer.listen({ port: PORT }, resolve));
-console.log(`🚀 Server ready at http://localhost:${PORT}/graphql`);
-await ConnectDB()
+    app.get('/playground', playground({ endpoint: '/graphql' }));
+    
+    await new Promise<void>(resolve => httpServer.listen({ port: PORT }, resolve));
+    console.log(`🚀 Server ready at http://localhost:${PORT}/graphql`);
+    await ConnectDB()
 }
 
 startServer()
